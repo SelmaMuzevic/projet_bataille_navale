80e1bf960fc66117e0ab97cebbb33ac63d97104d
selma <selma@gmail.com>
2017-09-13 10:03:32 +0200

    mis a jour

--- grid.js
+++ grid.js
@@ -8,6 +8,27 @@ for (let i = 0; i < 10; i++) {
             td.className = 'caseBataille';
             td.textContent = i + 1; //l'intérieur de la balise td = X//
             document.body.querySelector('.ligne' + i).appendChild(td);
+        } else {
+            let td = document.createElement('td');
+            td.className = 'caseBataille';
+            document.body.querySelector('.ligne' + i).appendChild(td);
+        }
+    }
+}
+for (let i = 0; i < 10; i++) {
+    let tr = document.createElement('tr');
+    tr.className = 'ligne' + i;
+    document.body.querySelector('#adversaire').appendChild(tr);
+    for (let j = 0; j < 11; j++) {
+        if (j == 0) {
+            let td = document.createElement('td');
+            td.className = 'caseBataille';
+            td.textContent = i + 1; //l'intérieur de la balise td = X//
+            document.body.querySelector('.ligne' + i).appendChild(td);
+        } else {
+            let td = document.createElement('td');
+            td.className = 'caseBataille';
+            document.body.querySelector('.ligne' + i).appendChild(td);
         }
     }
 }
